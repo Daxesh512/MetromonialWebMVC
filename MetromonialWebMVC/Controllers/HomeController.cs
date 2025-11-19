20ad12afe34bbd6f0209e8ece396fb7c5707cf4f
Daxesh512 <99954315+Daxesh512@users.noreply.github.com>
2025-11-19 23:55:15 +0530

    Metromonial

---
+++ MetromonialWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using MetromonialWebMVC.Models;
+
+namespace MetromonialWebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(TblUser userData)
+        {
+            using (MatroMinialDTEntities entities = new MatroMinialDTEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    entities.TblUsers.Add(userData);
+                    entities.SaveChanges();
+                    Response.Write("<script>alert('Registration Sucessfully')</script>");
+                    ModelState.Clear();
+                }
+            }
+            return View(userData);
+        }
+        [HttpPost]
+        public ActionResult Login(TblUser userData)
+        {
+            {
+                using (MatroMinialDTEntities entities = new MatroMinialDTEntities())
+                {
+                    var obj = entities.TblUsers.Where(a => a.userName.Equals(userData.userName) && a.password.Equals(userData.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        //Session["UserID"] = obj.UserId.ToString();
+                        //Session["UserName"] = obj.email.ToString();
+                        return RedirectToAction("Contact");
+                    }
+                }
+            }
+            return View(userData);
+        }
+
+    }
+}
---
+++ MetromonialWebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MetromonialWebMVC.MvcApplication" Language="C#" %>
